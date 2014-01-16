854be6d7d2e90f32065d6e7ab6dcc8f122e9cdf1
Kosuke Isobe <naughtydog0721@gmail.com>
2014-01-17 08:36:25 +0900

    exmaple/app.js replaced, like JVFloatLabeledTextField

--- example/app.js
+++ example/app.js
@@ -1,62 +1,137 @@
-// This is a test harness for your module
-// You should do something interesting in this harness 
-// to test out the module and to provide instructions 
-// to users on how to use it by example.
-
-
-// open a single window
-var win = Ti.UI.createWindow({
-	backgroundColor:'white'
-});
-win.open();
-
-// TODO: write your module tests here
 var TiJVFloatLabeledTextField = require('be.k0suke.tijvfloatlabeledtextfield');
 Ti.API.info("module is => " + TiJVFloatLabeledTextField);
 
-var textfield = TiJVFloatLabeledTextField.createTextField({
-	top: 40,
-	left: 10,
-	width: 300,
+var nextbutton = Ti.UI.createButton({
+	title: 'Next',
+	disabledColor: '#ccc',
+	enabled: false
+});
+
+var win = Ti.UI.createWindow({
+	title: 'Floating Label in Titanium',
+	rightNavButton: nextbutton,
+	backgroundColor: '#fff'
+});
+
+var nav = Ti.UI.iOS.createNavigationWindow({
+	window: win
+});
+nav.open();
+
+var titlefield = TiJVFloatLabeledTextField.createTextField({
+	top: 4,
+	left: 16,
+	width: Ti.UI.FILL,
 	height: 44,
-	value: 'default value',
 	font: {
 		fontSize: 16
 	},
 	color: '#333',
-	hintText: 'here is textfield',
+	value: '',
+	hintText: 'Title',
 	hintFont: {
 		fontSize: 12
 	},
 	hintColor: '#ccc',
-	activeHintColor: '#f00',
+	activeHintColor: '#c2111d',
 	clearButtonMode: Ti.UI.INPUT_BUTTONMODE_ONFOCUS
 });
-win.add(textfield);
+win.add(titlefield);
 
-textfield.addEventListener('change', function(e){
-	console.log(e);
-});
-
-var textarea = TiJVFloatLabeledTextField.createTextArea({
-	top: 104,
-	left: 10,
-	width: 300,
-	height: 200,
-	value: 'default value',
+var pricefield = TiJVFloatLabeledTextField.createTextField({
+	top: 53,
+	left: 16,
+	width: 60,
+	height: 44,
 	font: {
 		fontSize: 16
 	},
 	color: '#333',
-	hintText: 'here is textarea',
+	value: '',
+	hintText: 'Price',
 	hintFont: {
 		fontSize: 12
 	},
 	hintColor: '#ccc',
-	activeHintColor: '#f00'
+	activeHintColor: '#c2111d'
 });
-win.add(textarea);
+win.add(pricefield);
 
-textarea.addEventListener('change', function(e){
-	console.log(e);
+var locationfield = TiJVFloatLabeledTextField.createTextField({
+	top: 53,
+	left: 97,
+	width: Ti.UI.FILL,
+	height: 44,
+	font: {
+		fontSize: 16
+	},
+	color: '#333',
+	value: '',
+	hintText: 'Specific location (optional)',
+	hintFont: {
+		fontSize: 12
+	},
+	hintColor: '#ccc',
+	activeHintColor: '#c2111d'
 });
+win.add(locationfield);
+
+var descriptionarea = TiJVFloatLabeledTextField.createTextArea({
+	top: 102,
+	left: 16,
+	width: Ti.UI.FILL,
+	height: 132,
+	font: {
+		fontSize: 16
+	},
+	color: '#333',
+	value: '',
+	hintText: 'Description',
+	hintFont: {
+		fontSize: 12
+	},
+	hintColor: '#ccc',
+	activeHintColor: '#c2111d'
+});
+win.add(descriptionarea);
+
+win.add(Ti.UI.createView({
+	top: 49,
+	left: 16,
+	width: Ti.UI.FILL,
+	height: 1,
+	backgroundColor: '#ccc'
+}));
+
+win.add(Ti.UI.createView({
+	top: 50,
+	left: 81,
+	width: 1,
+	height: 48,
+	backgroundColor: '#ccc'
+}));
+
+win.add(Ti.UI.createView({
+	top: 98,
+	left: 16,
+	width: Ti.UI.FILL,
+	height: 1,
+	backgroundColor: '#ccc'
+}));
+
+function change(e) {
+	if (titlefield.getValue() !== '' &&
+		pricefield.getValue() !== '' &&
+		descriptionarea.getValue() !== '') {
+		nextbutton.setEnabled(true);
+	} else {
+		nextbutton.setEnabled(false);
+	}
+
+	win.setRightNavButton(nextbutton);
+}
+
+titlefield.addEventListener('change', change);
+pricefield.addEventListener('change', change);
+locationfield.addEventListener('change', change);
+descriptionarea.addEventListener('change', change);
